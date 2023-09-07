5e9982d3cf1e5c2f7b5ec04eab0e3e94554697fe
juanmaishere <rsjuanmanuel@hotmail.com>
2023-09-07 14:16:56 +0000

    feat

---
+++ 0-bubble_sort.c
@@ -0,0 +1,24 @@
+#include "sort.h"
+/**
+ * bubble_sort - the array to sorting
+ * @size: the size of the array
+*/
+void
+bubble_sort(int *array, size_t size)
+{
+    size_t i = 0, j = 0;
+    int temp = 0;
+    for (i = 0; i < size; i++)
+    {
+        for (j = 0; j < size; j++)
+        {
+            if (array[j] > array[j + 1])
+            {
+                temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+            }
+        }
+        print_array(array, size);
+    }
+}
