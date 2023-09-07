9c31480b18e11fb2cef97e1baa7b4c5e44bd85be
juanmaishere <rsjuanmanuel@hotmail.com>
2023-09-07 17:19:44 +0000

    feat

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -23,11 +23,11 @@ for (i = 0; i < size - 1; i++)
         array[j] = array[j + 1];
         array[j + 1] = temp;
         swap = 1;
+        print_array(array, size);
         }
     }
     if (swap == 0)
         break;
 
-print_array(array, size);
 }
 }
