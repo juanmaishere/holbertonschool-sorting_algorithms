a2094d03856769cecc99451e3d3cd737a0611724
NicoNethy <5761@holbertonstudents.com>
2023-09-13 09:32:40 -0700

    Betty fixes

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -2,30 +2,31 @@
 void
 selection_sort(int *array, size_t size)
 {
-    if (array == NULL || size < 2)
-        return;
+	int temp = 0;
+	size_t index = 0, i, j;
 
-    size_t index = 0, i, j;
-    int temp = 0;
-    
-    for (i = 0; i < size - 1; i++)
-    {
-        index = i;
-        
-        for (j = i + 1; j < size; j++)
-        {
-            if (array[j] < array[index])
-            {
-                index = j;
-            }
-        }
-        
-        if (index != i)
-        {
-            temp = array[i];
-            array[i] = array[index];
-            array[index] = temp;
-            print_array(array, size);
-        }
-    }
+	if (array == NULL || size < 2)
+	{
+		return;
+	}
+	for (i = 0; i < size - 1; i++)
+	{
+		index = i;
+
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[index])
+			{
+				index = j;
+			}
+		}
+
+		if (index != i)
+		{
+			temp = array[i];
+			array[i] = array[index];
+			array[index] = temp;
+			print_array(array, size);
+		}
+	}
 }
