7e0f1b82b34223658a49eaaa27be096a624c2ce3
NicoNethy <5761@holbertonstudents.com>
2023-09-13 09:24:30 -0700

    ; error

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -5,7 +5,7 @@ insertion_sort_list(listint_t **list)
 {
     listint_t *back;
     listint_t *current;
-    listint_t *next = current->next;
+    listint_t *next;
 
     if (list == NULL || *list == NULL || (*list)->next == NULL)
         return;
